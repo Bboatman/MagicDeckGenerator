d46108950466408f13922882bb47811b4c735f04
Brooke Boatman <bboatman1241@gmail.com>
2022-02-21 19:21:40 -0600

    updated init.py to properly download scryfall cards

--- init.py
+++ init.py
@@ -1,15 +1,7 @@
-import os, pip, sys, argparse
+import os, pip, json
 
 path = os.getcwd() 
 path += "/magicDeckGenerator/models"
-parser=argparse.ArgumentParser()
-
-parser.add_argument('--dbname', help='Psql Database Name')
-parser.add_argument('--dbhost', help='Database host location')
-parser.add_argument('--dbuser', help='Psql Username')
-parser.add_argument('--dbpass', help='Password for psql user')
-
-args=parser.parse_args(sys.argv[1:])
 
 try:  
     os.mkdir(path)
@@ -24,17 +16,14 @@ except SystemExit as e:
     pass
 
 import urllib.request
-from django.core.management.utils import get_random_secret_key
+from requests import get
 
 print('Beginning file download with urllib2...')
 
-url = 'https://archive.scryfall.com/json/scryfall-default-cards.json'  
-urllib.request.urlretrieve(url, path + "/scryfall-default-cards.json")  
-
-with open(".env", "w+") as f:
-    f.write("PGUSER={}\n".format(args.dbuser))
-    f.write("PGHOST={}\n".format(args.dbhost if args.dbhost else "localhost" ))
-    f.write("PGPASSWORD={}\n".format(args.dbpass))
-    f.write("PGDATABASE={}\n".format(args.dbname))
-    f.write("DJANGO_SECRET={}\n".format(get_random_secret_key()))
-    f.close()
+url = 'https://api.scryfall.com/bulk-data/oracle-cards' 
+try:
+    resp = get(url)
+    downloadLoc = json.loads(resp.text)["download_uri"]
+    urllib.request.urlretrieve(downloadLoc, path + "/scryfall-default-cards.json")  
+except:
+    print("Issue connecting to card ref download") 
