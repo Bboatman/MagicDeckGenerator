c0680f569b55ded0267f85b1e648ea2ea0a49260
Brooke Boatman <bboatman1241@gmail.com>
2023-04-30 02:35:19 -0500

    fixes keywords for doc2v, pickles cards as they are vectorized

--- magicDeckGenerator/lib/vectorize/main.py
+++ magicDeckGenerator/lib/vectorize/main.py
@@ -1,8 +1,17 @@
 import json
 import urllib.request
 from requests import get
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.test.utils import get_tmpfile
+import sys
+import pickle
+from rake_nltk import Rake
+import re
 
 card_path = './models/scryfall-default-cards.json'
+doc2vec_path = "./models/card_doc2vec"
+card_vec_path = './models/card_vecs.p'
+
 color_identities = {'': 0, 'C': 0, 'R': 1, 'U': 2, 'G': 3,
                     'B': 4, 'W': 5, 'RU': 6, 'GR': 7, 'BR': 8,
                     'RW': 9, 'GU': 10, 'BU': 11, 'UW': 12, 'BG': 13,
@@ -66,36 +75,102 @@ def convert_str_val_to_num(value):
         return float(value)
 
 
-def format_card_data(card, card_types, card_rarity):
+def format_card_data(card_json, card_types, card_rarity):
     try:
-        scryfall_link = card['scryfall_uri'].split('/')
+        scryfall_link = card_json['scryfall_uri'].split('/')
         card = {
-            'oracle_id': card['oracle_id'],
-            'tcgplayer_id': card.get('tcgplayer_id', None),
-            'mtgo_id': card.get('mtgo_id', None),
-            'arena_id': card.get('arena_id', None),
-            'cardmarket_id': card.get('cardmarket_id', None),
+            'oracle_id': card_json['oracle_id'],
+            'tcgplayer_id': card_json.get('tcgplayer_id', None),
+            'mtgo_id': card_json.get('mtgo_id', None),
+            'arena_id': card_json.get('arena_id', None),
+            'cardmarket_id': card_json.get('cardmarket_id', None),
             'scryfall_link_id': ''.join(scryfall_link[4:6]),
-            'name': card['name'],
-            'rarity': card_rarity.index(card['rarity']),
-            'card_type': card_types.index(card['type_line']),
-            'toughness': convert_str_val_to_num(card.get('toughness', None)),
-            'power': convert_str_val_to_num(card.get('power', None)),
-            'cmc': convert_str_val_to_num(card.get('cmc', None)),
-            'color_identity': color_identities[''.join(card['color_identity'])],
+            'name': card_json['name'],
+            'rarity': card_rarity.index(card_json['rarity']),
+            'card_type': card_types.index(card_json['type_line']),
+            'toughness': convert_str_val_to_num(card_json.get('toughness', None)),
+            'power': convert_str_val_to_num(card_json.get('power', None)),
+            'cmc': convert_str_val_to_num(card_json.get('cmc', None)),
+            'color_identity': color_identities[''.join(card_json['color_identity'])],
             'x': None,
-            'y': None
+            'y': None,
+            'text': card_json.get('oracle_text', ''),
+            'text_vec': None
         }
         return card
     except [KeyError, ValueError] as e:
         print(e, card['name'])
 
 
+def build_doc2vec_model(cards):
+    corpus = ''.join([card['text'].replace(
+        "//", " ") + " \n" for card in cards])
+    r = Rake()
+    r.extract_keywords_from_text(corpus)
+    tags = []
+    phrases = r.get_ranked_phrases_with_scores()
+    phrases.reverse()
+    for tag in phrases[:150000]:
+        if tag[1] not in tags and tag[0] > 1.0:
+            tags.append(tag[1])
+    documents = [TaggedDocument(doc['text'].split(" "), tags)
+                 for doc in cards]
+    model = Doc2Vec(documents, vector_size=4, window=2, min_count=1, workers=4)
+    model.save(doc2vec_path)
+
+
+def generate_text_vector(card):
+    model = Doc2Vec.load(doc2vec_path)
+    document = card['text'].split(" ")
+    vector = model.infer_vector(doc_words=document)
+    return vector
+
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
 if __name__ == "__main__":
+    commands = sys.argv
+    if 'build' in commands:
+        # load_cards()
+        print("Is building")
+
     data = get_card_json()
     card_types = get_card_type_list(data)
     card_rarity = get_card_rarity(data)
 
     formatted_cards = [format_card_data(
         x, card_types, card_rarity) for x in data]
-    print(formatted_cards[0])
+
+    try:
+        text_vec_cards = pickle.load(open(card_vec_path, 'rb'))
+    except [FileNotFoundError, EOFError]:
+        f = open(card_vec_path, "x")
+        text_vec_cards = formatted_cards
+
+    if ('build' in commands):
+        print('Building Model')
+        build_doc2vec_model(formatted_cards)
+
+    print('Generating text vectors')
+    process_count = 0
+    for x in range(len(formatted_cards)):
+        if text_vec_cards[x]['text_vec'] is None:
+            vector = generate_text_vector(formatted_cards[x])
+            formatted_cards[x]['text_vec'] = vector
+        if (process_count % 100 == 1):
+            printProgressBar(prefix=' Progress:', iteration=process_count/100, total=len(formatted_cards)/100)
+            pickle.dump(formatted_cards, open(card_vec_path, "wb"))
+
+        process_count += 1
+
+    if process_count > 0:
+        # If we processed any, do one extra dump for good measure to capture those missed in the % check
+        pickle.dump(formatted_cards, open(card_vec_path, "wb"))
